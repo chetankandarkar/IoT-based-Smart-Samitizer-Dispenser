a52cf1aa1819f7d095b0c013e0b96628cf9497f2
Chetan Kandarkar <74239460+chetankandarkar@users.noreply.github.com>
2021-05-24 20:54:31 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import conf
+from boltiot import Sms, Bolt
+import requests
+import json, time
+mybolt = Bolt(conf.API_KEY, conf.DEVICE_ID)
+sms = Sms(conf.SID, conf.AUTH_TOKEN, conf.TO_NUMBER,
+conf.FROM_NUMBER)
+while True:
+print("Reading sensor value")
+response = mybolt.digitalRead('0')
+data = json.loads(response)
+print("Value getting from Arduino : " + str(data['value']))
+try:
+sensor_value = int(data['value'])
+if sensor_value == 0:
+print("Making request to Twilio to send a SMS")
+response = sms.send_sms("Alert ! Entry detected without sanitization")
+print("Response received from Twilio is: " + str(response))
+print("Status of SMS at Twilio is :" + str(response.status))
+time.sleep(30)
+except Exception as e:
+print("Error occured: Below are the details")
+print(e)
+time.sleep(10)
